a1a11bd31f985f275af89ffe9a7b9d4a03c1168e
WDelenclos <wdelenclos@gmail.com>
2018-02-19 19:11:23 +0100

    touristic

--- Tools/generatePlageBase.php
+++ Tools/generatePlageBase.php
@@ -8,15 +8,16 @@ $taillePlageSec = $tailleDesPlages*3600;
 $max = $nbDeJours*(24/$tailleDesPlages);
 $last=$dateDuDebut;
 $baseOfPlages = [];
-$obj= [1,2,3,4,5,11,12,13,14,15,21,22,23,24,25,31,32,33,34,35,41,42,43,44,45,51,52,53,54,55,61,62,63,64,65,71,72,73,74,75,81,82,83,84,85,91,92,93,94,95];
+
+$DaysToExclude= ['00','02','04', '06', '08','22'];
+
 for ($i = 0; $i <= $max; $i++){
-    if (in_array($i, $obj)){
-       // do nothing;
+    if(in_array(date('H', $last),$DaysToExclude)){
+    // Do nothing FDP
+    }else{
+        array_push($baseOfPlages, $last);
     }
-    else{
-    array_push($baseOfPlages,$last);
     $last = $last+$taillePlageSec;
-    }
 }
 echo(json_encode($baseOfPlages));
 
